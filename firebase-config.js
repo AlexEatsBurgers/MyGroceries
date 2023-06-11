344d6b4a51bb9e0661176c5d7732da821673b24e
AlexEatsBurgers <125730192+AlexEatsBurgers@users.noreply.github.com>
2023-06-11 19:09:34 +1000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,16 @@
-import { initializeApp } from "firebase/app"
-import { getDatabase } from "firebase/database";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: `${process.env.VITE_API_KEY}`,
-    authDomain: `${process.env.VITE_AUTH_DOMAIN}`,
-    databaseURL: `${process.env.VITE_DATABASE_URL}`,
-    projectId: `${process.env.VITE_PROJECT_ID}`,
-    storageBucket: `${process.env.VITE_STORAGE_BUCKET}`,
-    messagingSenderId: `${process.env.VITE_MESSAGING_SENDER_ID}`,
-    appId: `${process.env.VITE_APP_ID}`
-  }
+  apiKey: process.env.VITE_API_KEY,
+  authDomain: process.env.VITE_AUTH_DOMAIN,
+  projectId: process.env.VITE_PROJECT_ID,
+  storageBucket: process.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_APP_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 export const database = getDatabase(app);
-export const auth = getAuth(app)
-
+export const auth = getAuth(app);
